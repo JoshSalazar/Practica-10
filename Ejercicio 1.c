2d5145a85dfa270933996ecfb508c1a5679ab107
JoshSalazar <Salazar5a10j2@outlook.com>
2024-10-18 13:37:25 -0600

    Create Ejercicio 1.c

---
+++ Ejercicio 1.c
@@ -0,0 +1,20 @@
+/*Salazar Anicasio Joshua
+ejercicio 1 matriz 3x3
+Práctica 10 18/10/24*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int arr[3][3] = {{1, 2, 3} , {1, 2, 3} , {1, 2, 3}};
+  int i, j;
+  
+  for(i = 0; i < 3; i++)
+  {
+    for(j = 0; j<3; j++)
+    {
+      printf("%d", arr[i][j]);
+    }
+  }
+}
