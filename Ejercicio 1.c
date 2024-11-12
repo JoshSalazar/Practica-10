9548d995abf50cb20f9b542f962a0d23d2a3c530
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-12 14:30:59 -0600

    Update and rename Ejercicio 1-f .c to Ejercicio 1.c

--- Ejercicio 1.c
+++ Ejercicio 1.c
@@ -3,19 +3,22 @@ ejercicio 1 matriz 3x3
 Práctica 10 18/10/24*/
 
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
   int arr[3][3] = {{1, 2, 3} , {1, 2, 3} , {1, 2, 3}};
   int i, j;
+
+  printf("Imprimir matriz.\n");
   
   for(i = 0; i < 3; i++)
   {
-    printf("\n");
     for(j = 0; j<3; j++)
     {
-      printf("\t%d", arr[i][j]);
+      printf("%d | ", arr[i][j]);
     }
+  
+    printf("\n");  
   }
+  return 0;
 }
